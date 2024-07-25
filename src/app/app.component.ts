9aebd1470b2017a09f000e3fbdcc85ad4472b3ad
Tim Van de Cruys <tim.vandecruys@kuleuven.be>
2024-07-25 14:07:25 +0200

    general update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,24 +1,11 @@
 import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 import { Router } from '@angular/router';
-import { SwUpdate } from '@angular/service-worker';
-
-import { MenuController, Platform, ToastController } from '@ionic/angular';
-
+import { MenuController, Platform } from '@ionic/angular';
 import { StatusBar } from '@capacitor/status-bar';
 import { SplashScreen } from '@capacitor/splash-screen';
-
 import { Storage } from '@ionic/storage-angular';
-
-
 import { UserData } from './providers/user-data';
 
-/* import {
-  ActionPerformed,
-  PushNotificationSchema,
-  PushNotifications,
-  Token,
-} from '@capacitor/push-notifications';
- */
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -37,17 +24,6 @@ export class AppComponent implements OnInit {
       url: '/app/tabs/schedule',
       icon: 'calendar'
     },
-/*   {
-      title: 'City Map',
-      url: '/app/tabs/map',
-      icon: 'map'
-    },
-    {
-      title: 'Campus Map',
-      url: '/app/tabs/campus-map',
-      icon: 'navigate'
-    },
- */
     {
       title: 'Maps',
       url: '',
@@ -81,77 +57,23 @@ export class AppComponent implements OnInit {
     private router: Router,
     private storage: Storage,
     private userData: UserData,
-    private swUpdate: SwUpdate,
-    private toastCtrl: ToastController,
   ) {
     this.initializeApp();
   }
 
   async ngOnInit() {
     try {
-      console.log("starting app component")
+      console.log("starting app component");
       await this.storage.create();
       // this.checkLoginStatus();
       // this.listenForLoginEvents();
-  
-      this.swUpdate.versionUpdates.subscribe(async res => {
-        const toast = await this.toastCtrl.create({
-          message: 'Update available!',
-          position: 'bottom',
-          buttons: [
-            {
-              role: 'cancel',
-              text: 'Reload'
-            }
-          ]
-        });
-  
-        await toast.present();
-  
-        toast.onDidDismiss().then(() => {
-          this.swUpdate.activateUpdate().then(() => {
-            window.location.reload();
-          });
-        });
-      });
-  
-      // this.initPushNotifications();
     } catch (error) {
       console.error('Error during ngOnInit:', error);
     }
   }
 
-  /* async ngOnInit() {
-    await this.storage.create();
-    //this.checkLoginStatus();
-    //this.listenForLoginEvents();
-
-    this.swUpdate.versionUpdates.subscribe(async res => {
-      const toast = await this.toastCtrl.create({
-        message: 'Update available!',
-        position: 'bottom',
-        buttons: [
-          {
-            role: 'cancel',
-            text: 'Reload'
-          }
-        ]
-      });
-
-      await toast.present();
-
-      toast
-        .onDidDismiss()
-        .then(() => this.swUpdate.activateUpdate())
-        .then(() => window.location.reload());
-    });
-
-    //this.initPushNotifications();
-
-  }
- */
   initializeApp() {
-    console.log ('initializing app')
+    console.log('initializing app');
     this.platform.ready().then(() => {
       if (this.platform.is('hybrid')) {
         StatusBar.hide();
@@ -162,14 +84,17 @@ export class AppComponent implements OnInit {
   }
 
   handleBackButton() {
-    const newsUrl = '/app/tabs/news';
+    const defaultUrl = '/app/tabs/news';
+    const scheduleUrl = '/app/tabs/schedule';
 
     this.platform.backButton.subscribeWithPriority(10, (processNextHandler) => {
       const currentUrl = this.router.url;
-      if (currentUrl === newsUrl) {
+      if (currentUrl === defaultUrl) {
         navigator['app'].exitApp(); // Close the app
+      } else if (currentUrl.startsWith('/app/tabs/schedule/session/')) {
+        this.router.navigateByUrl(scheduleUrl); // Navigate to the 'Schedule' tab
       } else if (currentUrl.startsWith('/app/tabs')) {
-        this.router.navigateByUrl(newsUrl); // Navigate to the 'News' tab
+        this.router.navigateByUrl(defaultUrl); // Navigate to the 'News' tab
       } else {
         // Allow the default back button action
         processNextHandler();
@@ -180,80 +105,4 @@ export class AppComponent implements OnInit {
   toggleSubmenu() {
     this.submenuOpen = !this.submenuOpen;
   }
-
-  /* checkLoginStatus() {
-    return this.userData.isLoggedIn().then(loggedIn => {
-      return this.updateLoggedInStatus(loggedIn);
-    });
-  }
-
-  updateLoggedInStatus(loggedIn: boolean) {
-    setTimeout(() => {
-      this.loggedIn = loggedIn;
-    }, 300);
-  }
-
-  listenForLoginEvents() {
-    window.addEventListener('user:login', () => {
-      this.updateLoggedInStatus(true);
-    });
-
-    window.addEventListener('user:signup', () => {
-      this.updateLoggedInStatus(true);
-    });
-
-    window.addEventListener('user:logout', () => {
-      this.updateLoggedInStatus(false);
-    });
-  }
-
-  logout() {
-    this.userData.logout().then(() => {
-      return this.router.navigateByUrl('/app/tabs/schedule');
-    });
-  } */
-
-  /* openTutorial() {
-    this.menu.enable(false);
-    this.storage.set('ion_did_tutorial', false);
-    this.router.navigateByUrl('/tutorial');
-  } */
-
-  /* initPushNotifications() {
-    console.log('initializing push notifications')
-    // Request permission to use push notifications
-    // iOS will prompt user and return if they granted permission or not
-    // Android will just grant without prompting
-    PushNotifications.requestPermissions().then(result => {
-      if (result.receive === 'granted') {
-        // Register with Apple / Google to receive push via APNS/FCM
-        PushNotifications.register();
-      } else {
-        // Show some error
-      }
-    });
-
-    PushNotifications.addListener('registration', (token: Token) => {
-      alert('Push registration success, token: ' + token.value);
-    });
-
-    PushNotifications.addListener('registrationError', (error: any) => {
-      alert('Error on registration: ' + JSON.stringify(error));
-    });
-
-    PushNotifications.addListener(
-      'pushNotificationReceived',
-      (notification: PushNotificationSchema) => {
-        alert('Push received: ' + JSON.stringify(notification));
-      },
-    );
-
-    PushNotifications.addListener(
-      'pushNotificationActionPerformed',
-      (notification: ActionPerformed) => {
-        alert('Push action performed: ' + JSON.stringify(notification));
-      },
-    );
-  } */
-
 }
